dc4fafd641e631aed843f1b570ceae8a423b231e
AkhilJayan <mailtoakhiljayan@gmail.com>
2017-05-02 15:13:50 +0800

    initial commit

---
+++ ApplicationDeployToolsWCF/IService1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ApplicationDeployToolsWCF
+{
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/sites", ResponseFormat = WebMessageFormat.Json)]
+        List<string> GetAllSites();
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/pools", ResponseFormat = WebMessageFormat.Json)]
+        List<string> GetAllPools();
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/app/{site}", ResponseFormat = WebMessageFormat.Json)]
+        List<string> GetAppBySite(string site);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/addApp/{appPool}/{app}", ResponseFormat = WebMessageFormat.Json)]
+        string SetAppPool(string appPool, string app, string site);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/move/change/{app}/{path}/{site}", ResponseFormat = WebMessageFormat.Json)]
+        string MoveChangeAppPath(string app, string path, string site);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/getpath/{app}/{site}", ResponseFormat = WebMessageFormat.Json)]
+        string GetAppVirtualPath(string app, string site);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/createPool/{name}/{netclr}/{pipeline}/{startflag}", ResponseFormat = WebMessageFormat.Json)]
+        string CreateNewPool(string name, string netclr, string pipeline, bool startflag);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/createAppVir/{site}/{pool}/{name}/{virtualpath}/{appvir}", ResponseFormat = WebMessageFormat.Json)]
+        string CreateAppVir(string site, string pool, string name, string virtualpath, string appvir);
+    }
+
+
+    [DataContract]
+    public class WCFSites
+    {
+        string siteName;
+
+        public static WCFSites MakeSite(string siteName)
+        {
+            WCFSites s = new WCFSites();
+            s.siteName = siteName;
+            return s;
+        }
+
+        [DataMember]
+        public string SiteName
+        {
+            get { return siteName; }
+            set { siteName = value; }
+        }
+    }
+
+    [DataContract]
+    public class WCFPools
+    {
+        string poolName;
+
+        public static WCFPools MakePool(string poolName)
+        {
+            WCFPools p = new WCFPools();
+            p.poolName = poolName;
+            return p;
+        }
+
+        [DataMember]
+        public string PoolName
+        {
+            get { return poolName; }
+            set { poolName = value; }
+        }
+    }
+
+    [DataContract]
+    public class WCFSiteApp
+    {
+        string siteAppName;
+
+        public static WCFSiteApp MakeSiteApp(string siteAppName)
+        {
+            WCFSiteApp sa = new WCFSiteApp();
+            sa.siteAppName = siteAppName;
+            return sa;
+        }
+
+        [DataMember]
+        public string SiteAppName
+        {
+            get { return siteAppName; }
+            set { siteAppName = value; }
+        }
+    }
+}
---
+++ ApplicationDeployToolsWCF/Service1.svc.cs
@@ -0,0 +1,220 @@
+﻿using Microsoft.Web.Administration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ApplicationDeployToolsWCF
+{
+    public class Service1 : IService1
+    {
+        [OperationBehavior(Impersonation = ImpersonationOption.Required)]
+        public string CreateAppVir(string site, string pool, string name, string virtualpath, string appvir)
+        {
+            ServerManager serverManager = new ServerManager();
+            Site s = null;
+            ApplicationPool ap = null;
+            foreach (Site mainsite in serverManager.Sites)
+            {
+                if (mainsite.Name.ToString() == site)
+                {
+                    s = mainsite;
+                }
+            }
+            foreach (ApplicationPool appPool in serverManager.ApplicationPools)
+            {
+                if (appPool.Name.ToString() == pool)
+                {
+                    ap = appPool;
+                }
+            }
+            Application rootApp = s.Applications.First(a => a.Path == "/");
+            rootApp.VirtualDirectories.Add("/" + name, @"" + virtualpath);
+            if (appvir == "app")
+            {
+                rootApp.ApplicationPoolName = ap.Name.ToString();
+            }
+            serverManager.CommitChanges();
+
+            string path = @"" + virtualpath;
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            return "OK";
+        }
+
+        [OperationBehavior(Impersonation = ImpersonationOption.Required)]
+        public string CreateNewPool(string name, string netclr, string pipeline, bool startflag)
+        {
+            ManagedPipelineMode mode = ManagedPipelineMode.Classic; ;
+            ServerManager serverManager = new ServerManager();
+            ApplicationPool newPool = serverManager.ApplicationPools.Add(name);
+            newPool.ManagedRuntimeVersion = netclr;
+            if (pipeline == "Integrated")
+            {
+                mode = ManagedPipelineMode.Integrated;
+            }
+            else if (pipeline == "Clasic")
+            {
+                mode = ManagedPipelineMode.Classic;
+            }
+            newPool.ManagedPipelineMode = mode;
+            newPool.Enable32BitAppOnWin64 = startflag;
+            serverManager.CommitChanges();
+            return "OK";
+        }
+
+        [OperationBehavior(Impersonation = ImpersonationOption.Required)]
+        public List<string> GetAllPools()
+        {
+            ServerManager serverManager = new ServerManager();
+            List<string> listPools = new List<string>();
+            foreach (ApplicationPool appPool in serverManager.ApplicationPools)
+            {
+                string poolName = appPool.Name.ToString();
+                string identityType = appPool.ProcessModel.IdentityType.ToString();
+                listPools.Add(poolName);
+            }
+
+            return listPools;
+        }
+
+        [OperationBehavior(Impersonation = ImpersonationOption.Required)]
+        public List<string> GetAllSites()
+        {
+            ServerManager serverManager = new ServerManager();
+            List<string> listSites = new List<string>();
+            foreach (Site site in serverManager.Sites)
+            {
+                string siteName = site.Name.ToString();
+                listSites.Add(siteName);
+            }
+            return listSites;
+        }
+
+        [OperationBehavior(Impersonation = ImpersonationOption.Required)]
+        public List<string> GetAppBySite(string site)
+        {
+            ServerManager serverManager = new ServerManager();
+            List<string> listApps = new List<string>();
+            foreach (Site mainsite in serverManager.Sites)
+            {
+                if (mainsite.Name.ToString() == site)
+                {
+                    foreach (var item in mainsite.Applications)
+                    {
+                        string appName = item.Path.Substring(1);
+                        listApps.Add(appName);
+                    }
+                }
+            }
+            return listApps;
+        }
+
+        [OperationBehavior(Impersonation = ImpersonationOption.Required)]
+        public string GetAppVirtualPath(string app, string site)
+        {
+            string path = "";
+            ServerManager serverManager = new ServerManager();
+            foreach (Site insite in serverManager.Sites)
+            {
+                if (insite.Name.ToString() == site)
+                {
+                    foreach (var item in insite.Applications)
+                    {
+                        if (item.Path.Substring(1) == app)
+                        {
+                            path = item.VirtualDirectories["/"].PhysicalPath;
+                        }
+                    }
+                    serverManager.CommitChanges();
+                }
+            }
+            return path;
+        }
+
+        [OperationBehavior(Impersonation = ImpersonationOption.Required)]
+        public string MoveChangeAppPath(string app, string path, string site)
+        {
+            //string rootdir = ConfigurationManager.AppSettings["rootDirectory"];
+            //string sourceFile = @""+rootdir+"\\"+ app;
+            string src = this.GetAppVirtualPath(app, site);
+            string sourceFile = @"" + src.Replace("%SystemDrive%", "C:");
+
+            string destinationFile = @"" + path;
+            Microsoft.VisualBasic.FileIO.FileSystem.MoveDirectory(sourceFile, destinationFile, true);
+
+
+            ServerManager serverManager = new ServerManager();
+            foreach (Site insite in serverManager.Sites)
+            {
+                if (insite.Name.ToString() == site)
+                {
+                    foreach (var item in insite.Applications)
+                    {
+                        if (item.Path.Substring(1) == app)
+                        {
+                            item.VirtualDirectories["/"].PhysicalPath = @"" + path;
+                        }
+                    }
+                    serverManager.CommitChanges();
+                }
+            }
+
+            return "OK";
+        }
+
+        [OperationBehavior(Impersonation = ImpersonationOption.Required)]
+        public string SetAppPool(string appPool, string app, string site)
+        {
+            ServerManager serverManager = new ServerManager();
+            ApplicationPool resultpool = null;
+
+            foreach (ApplicationPool pool in serverManager.ApplicationPools)
+            {
+                if (pool.Name.ToString() == appPool)
+                {
+                    resultpool = pool;
+                }
+            }
+
+            foreach (Site insite in serverManager.Sites)
+            {
+                if (insite.Name.ToString() == site)
+                {
+                    //insite.Stop();
+                    foreach (var item in insite.Applications)
+                    {
+                        if (item.Path.Substring(1) == app)
+                        {
+                            item.ApplicationPoolName = resultpool.Name;
+                        }
+                    }
+                    serverManager.CommitChanges();
+                    //insite.Start();
+                    resultpool.Recycle();
+                }
+            }
+
+            //foreach (Site mainsite in serverManager.Sites)
+            //{
+            //    foreach (var item in mainsite.Applications)
+            //    {
+            //        if (item.Path.Substring(1) == app)
+            //        {
+            //            item.ApplicationPoolName = resultpool.Name;
+            //        }
+            //    }
+            //}
+
+            return "OK";
+
+        }
+    }
+}
